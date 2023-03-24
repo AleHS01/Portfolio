51a821e4a1fcd687db07944ba8a479071980a450
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-03-24 00:29:15 -0400

    adding alert with JS

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+
+// Delaying the apperance of the alert dialog box, so the page load then the dialog box
+setTimeout(()=> {
+    alert("This Wesbite it's not responsive yet! By the Moment only open it on Computer/Laptop Browser");
+ }
+ ,2000);
