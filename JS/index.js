82cd6f3b5c554062284da730d869d4b71fb867af
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-04-20 21:44:08 -0400

    phone responsive done

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
 let nav = document.getElementById('navbar');
-nav.style.transition = "top 0.8s ease-out";
+// nav.style.transition = "top 0.8s ease-out";
 let pagePrevPos = window.scrollY + 50;
 
 window.addEventListener('scroll', function(){
@@ -16,13 +16,15 @@ window.addEventListener('scroll', function(){
 });
  
 //Deleting the PopAnimation after 8 secs
-
-setTimeout(()=>{
-    let popUp = document.getElementById("pop-up");
-    popUp.style.transition = "opacity 1s linear";
-    popUp.style.opacity = "0";
-    setTimeout(() =>{
-        popUp.style.display = "none"; //Removing the element fromt the document flow
-    },1000)
-    
-},4000);
+if(window.location.pathname === "/about-me.html"){ //if we are in about me page then target the pop up
+    setTimeout(()=>{
+        let popUp = document.getElementById("pop-up");
+        popUp.style.transition = "opacity 1s linear";
+        popUp.style.opacity = "0";
+        popUp.style.we
+        setTimeout(() =>{
+            popUp.style.display = "none"; //Removing the element fromt the document flow
+        },1000)
+        
+    },4000);
+}
